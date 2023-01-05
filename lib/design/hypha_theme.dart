d35cf6f6beeedb627f91f9637cfd8f88d2516f48
Gerardo Guijarro <gguijarro_c@chewy.com>
2023-01-05 09:52:37 -0600

    Google fonts packages

--- lib/design/hypha_theme.dart
+++ lib/design/hypha_theme.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hypha_wallet/design/hypha_colors.dart';
 
 class HyphaTheme {
   static ThemeData get darkTheme {
+    var mainTextTheme = GoogleFonts.ralewayTextTheme();
+
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: HyphaColors.green).copyWith(
         brightness: Brightness.dark,
+      ),
+      textTheme: mainTextTheme.copyWith(
+        button: GoogleFonts.poppins(textStyle: mainTextTheme.button),
       ),
       extensions: const <ThemeExtension<dynamic>>[],
     );
   }
 
   static ThemeData get lightTheme {
+    var mainTextTheme = GoogleFonts.ralewayTextTheme();
+
     return ThemeData(
       colorScheme: ColorScheme.fromSeed(seedColor: HyphaColors.red).copyWith(
         brightness: Brightness.light,
+      ),
+      textTheme: mainTextTheme.copyWith(
+        button: GoogleFonts.poppins(textStyle: mainTextTheme.button),
       ),
       extensions: const <ThemeExtension<dynamic>>[],
     );
