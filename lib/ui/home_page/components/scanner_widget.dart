5805f15ba2ecf67552c64e125844309d75fd707f
Gerardo Guijarro <gguijarro_c@chewy.com>
2023-01-12 18:53:32 -0600

    Add animation

--- lib/ui/home_page/components/scanner_widget.dart
+++ lib/ui/home_page/components/scanner_widget.dart
@@ -6,6 +6,7 @@ import 'package:get/get.dart';
 import 'package:hypha_wallet/core/error_handler/model/hypha_error.dart';
 import 'package:hypha_wallet/core/error_handler/model/hypha_error_type.dart';
 import 'package:hypha_wallet/core/logging/log_helper.dart';
+import 'package:hypha_wallet/design/hypha_colors.dart';
 import 'package:hypha_wallet/design/themes/extensions/theme_extension_provider.dart';
 import 'package:hypha_wallet/ui/blocs/error_handler/error_handler_bloc.dart';
 import 'package:hypha_wallet/ui/home_page/interactor/home_bloc.dart';
@@ -20,6 +21,10 @@ class ScannerWidget extends StatefulWidget {
 
 class _ScannerWidgetState extends State<ScannerWidget> {
   bool isActive = false;
+  double _width = 50;
+  double _heightFactor = .30;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   Widget build(BuildContext context) {
@@ -30,16 +35,19 @@ class _ScannerWidgetState extends State<ScannerWidget> {
               // context.read<HomeBloc>().add(HomeEvent.onQRCodeScanned('code'));
               setState(() {
                 isActive = true;
+                _heightFactor = 0.40;
               });
             },
       child: ClipRRect(
         borderRadius: BorderRadius.circular(30),
-        child: Container(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.fastOutSlowIn,
           decoration: BoxDecoration(
             image: DecorationImage(image: AssetImage('assets/images/graphics/qr_back.png'), fit: BoxFit.fill),
             borderRadius: BorderRadius.circular(30),
           ),
-          height: Get.height * .30,
+          height: Get.height * _heightFactor,
           child: BackdropFilter(
             filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
             child: isActive
@@ -76,9 +84,10 @@ class _ScannerWidgetState extends State<ScannerWidget> {
                           onPressed: () {
                             setState(() {
                               isActive = false;
+                              _heightFactor = 0.30;
                             });
                           },
-                          icon: Icon(Icons.add_circle, size: 36),
+                          icon: Icon(Icons.add_circle, size: 36, color: HyphaColors.primaryBlu),
                         ),
                         alignment: Alignment.bottomCenter,
                       ),
@@ -88,9 +97,9 @@ class _ScannerWidgetState extends State<ScannerWidget> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(Icons.qr_code_scanner, size: 60),
+                        Icon(Icons.qr_code_scanner, size: 80, color: HyphaColors.primaryBlu),
                         SizedBox(height: 12),
-                        Text('Scan QR', style: context.hyphaTextTheme.smallTitles),
+                        Text('Scan QR', style: context.hyphaTextTheme.smallTitles.copyWith(color: HyphaColors.white)),
                       ],
                     ),
                   ),
