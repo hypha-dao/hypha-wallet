f1dca0a98b7735a866d4afca07bdf01745f346b8
NIK <nheger@gmail.com>
2023-01-19 13:26:36 +0700

    sanitize parameter values, cleanup
    add stack trace to error mapper
    use typed parameters for overall sanity - this is not JavaScript!

--- lib/core/network/api/eos_service.dart
+++ lib/core/network/api/eos_service.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 import 'package:async/async.dart';
+import 'package:dio/dio.dart' as dio;
 import 'package:hypha_wallet/core/crypto/eosdart/eosdart.dart';
 import 'package:hypha_wallet/core/crypto/seeds_esr/eos_transaction.dart';
-import 'package:hypha_wallet/core/local/models/user_auth_data.dart';
-import 'package:hypha_wallet/core/local/services/secure_storage_service.dart';
 import 'package:hypha_wallet/core/logging/log_helper.dart';
 
 String onboardingPrivateKey = '5JhM4vypLzLdDtHo67TR5RtmsYm2mr8F2ugqcrCzfrMPLvo8cQW';
@@ -38,32 +37,30 @@ class EOSService {
         ];
       }
     }
-    LogHelper.d('GERY GERY: sendTransaction Done wit if: ');
-
-    LogHelper.d('GERY GERY: sendTransaction Action: ' + actions.toString());
-
     final transaction = _buildTransaction(actions, accountName);
 
     final UserAuthData? userAuthData = await secureStorageService.getUserAuthData();
     LogHelper.d('GERY GERY: ${userAuthData?.eOSPrivateKey?.toString()}');
 
-    return EOSClient(baseUrl: 'http://eos.greymass.com', privateKeys: ['NIK ENTER YOUR KEY'], version: 'v1')
+    // ignore: prefer_interpolation_to_compose_strings
+    print('GERY GERY: sendTransaction : ' + transaction.toJson().toString());
+
+    // final dio.Response resp = await eosClient.pushTransaction(transaction);
+
+    return eosClient
         .pushTransaction(transaction)
-        .then((dynamic response) => _mapEosResponse(response, (dynamic map) {
-              return response['transaction_id'];
+        .then((dio.Response response) => _mapEosResponse(response, (dynamic map) {
+              return map['transaction_id'];
             }))
-        .catchError((error) => _mapEosError(error));
+        .catchError((error, s) => _mapEosError(error, s));
   }
 
-  FutureOr<Result<T>> _mapEosResponse<T>(dynamic response, Function modelMapper) {
-    print('mapEosResponse - transaction id: ${response['transaction_id']}');
-    if (response['transaction_id'] != null) {
-      print('Model Class: $modelMapper');
-      final map = Map<String, dynamic>.from(response);
+  FutureOr<Result<T>> _mapEosResponse<T>(dio.Response response, Function modelMapper) {
+    if (response.data['transaction_id'] != null) {
+      final map = Map<String, dynamic>.from(response.data);
       return ValueResult(modelMapper(map));
     } else {
-      print('ErrorResult: $response');
-      return ErrorResult(EosError(response['processed']['error_code']));
+      return ErrorResult(EosError(response.data['processed']['error_code']));
     }
   }
 
@@ -76,8 +73,11 @@ class EOSService {
     return transaction;
   }
 
-  ErrorResult _mapEosError(dynamic error) {
+  ErrorResult _mapEosError(dynamic error, StackTrace? s) {
     print('mapEosError: $error');
+    if (s != null) {
+      print('stack: $s');
+    }
     return ErrorResult(error);
   }
 }
