43b0521e9f95f45d76c3ec342100317f65ff9892
NIK <nheger@gmail.com>
2023-02-16 00:35:57 +0800

    dio fix

--- lib/core/network/repository/profile_repository.dart
+++ lib/core/network/repository/profile_repository.dart
@@ -1,19 +1,18 @@
-import 'package:dio/dio.dart';
 import 'package:hypha_wallet/core/error_handler/model/hypha_error.dart';
 import 'package:hypha_wallet/core/error_handler/model/hypha_error_type.dart';
 import 'package:hypha_wallet/core/network/api/endpoints.dart';
+import 'package:hypha_wallet/core/network/networking_manager.dart';
 import 'package:hypha_wallet/ui/architecture/result/result.dart';
 import 'package:hypha_wallet/ui/profile/interactor/profile_data.dart';
 
-class ProfileService {
+class ProfileService extends NetworkingManager {
+  ProfileService() : super(Endpoints.profileServiceEndpoint);
+
   Future<Result<ProfileData, HyphaError>> getProfile(String accountName) async {
-    final dio = Dio();
     final url = '${Endpoints.profileServiceEndpoint}${Endpoints.pppProfile}/$accountName';
-    final response = await dio.get(url);
+    final response = await get(url);
     if (response.statusCode == 200) {
       final map = Map<String, dynamic>.from(response.data);
-      final profile = ProfileData.fromJson(map);
-      // print("proflle image ${profile.image}");
       return Result.value(ProfileData.fromJson(map));
     } else {
       print('get profile error: ${response.statusCode} ${response.statusMessage}');
