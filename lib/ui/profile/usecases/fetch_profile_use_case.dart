ad00ea00297cfb1b11ffe99d781705c8e4c8eb10
NIK <nheger@gmail.com>
2023-02-16 23:09:06 +0800

    fix import

--- lib/ui/profile/usecases/fetch_profile_use_case.dart
+++ lib/ui/profile/usecases/fetch_profile_use_case.dart
@@ -1,7 +1,6 @@
 import 'package:hypha_wallet/core/error_handler/model/hypha_error.dart';
 import 'package:hypha_wallet/core/network/repository/profile_repository.dart';
 import 'package:hypha_wallet/core/ppp_service/ppp.dart';
-import 'package:hypha_wallet/core/shared_preferences/hypha_shared_prefs.dart';
 import 'package:hypha_wallet/ui/architecture/interactor/base_usecase.dart';
 import 'package:hypha_wallet/ui/architecture/result/result.dart';
 import 'package:hypha_wallet/ui/profile/interactor/profile_data.dart';
