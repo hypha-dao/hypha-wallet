7294d1f0448cf69548b6e4c95bf27d67e3a68714
NIK <nheger@gmail.com>
2023-04-10 16:23:36 +0800

    set transaction id simplified

--- lib/core/network/api/sign_transaction_callback_service.dart
+++ lib/core/network/api/sign_transaction_callback_service.dart
@@ -4,7 +4,6 @@ import 'package:dio/dio.dart';
 import 'package:hypha_wallet/core/network/networking_manager.dart';
 
 class SignTransactionCallbackService {
-  final String transactionIdReplacementString = '{{tx}}';
   final String transactionIdQueryParameterName = 'tx_id';
 
   final NetworkingManager _networkingManager;
@@ -14,9 +13,8 @@ class SignTransactionCallbackService {
   Future<Response> callTheCallback(String callback, String transactionId) async {
     final uri = Uri.parse(callback);
     final queryParameters = Map.from(uri.queryParameters);
-    if (queryParameters[transactionIdQueryParameterName] == transactionIdReplacementString) {
-      queryParameters[transactionIdQueryParameterName] = transactionId;
-    }
+    queryParameters[transactionIdQueryParameterName] = transactionId;
+
     final body = jsonEncode(queryParameters);
     final path = uri.replace(query: '');
 
