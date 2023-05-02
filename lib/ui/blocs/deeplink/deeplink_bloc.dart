de3a2534acbef83acf5cd2b8cf10f5589d0075ab
NIK <nheger@gmail.com>
2023-05-02 19:15:17 +0800

    esr support intermediary check in

--- lib/ui/blocs/deeplink/deeplink_bloc.dart
+++ lib/ui/blocs/deeplink/deeplink_bloc.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:app_links/app_links.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:get/get.dart';
+import 'package:hypha_wallet/core/crypto/dart_esr/dart_esr.dart';
 import 'package:hypha_wallet/core/crypto/seeds_esr/scan_qr_code_result_data.dart';
 import 'package:hypha_wallet/core/logging/log_helper.dart';
 import 'package:hypha_wallet/ui/home_page/usecases/parse_qr_code_use_case.dart';
@@ -14,6 +17,10 @@ part 'page_command.dart';
 
 class DeeplinkBloc extends Bloc<DeeplinkEvent, DeeplinkState> {
   final ParseQRCodeUseCase _parseQRCodeUseCase;
+  final _appLinks = AppLinks();
+
+  StreamSubscription<String>? _linkSubscription;
+
   DeeplinkBloc(this._parseQRCodeUseCase) : super(const DeeplinkState()) {
     initDynamicLinks();
 
@@ -22,17 +29,58 @@ class DeeplinkBloc extends Bloc<DeeplinkEvent, DeeplinkState> {
     on<_ClearPageCommand>((_, emit) => emit(state.copyWith(command: null)));
   }
 
-  Future<void> initDynamicLinks() async {
-    final PendingDynamicLinkData? initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
+  void _debugShowMessage(String message) {
+    Get.showSnackbar(GetSnackBar(message: message, duration: const Duration(seconds: 5)));
+  }
 
-    if (initialLink != null) {
-      LogHelper.d('initial link: ${initialLink.link}');
-      add(DeeplinkEvent.incomingFirebaseDeepLink(initialLink.link));
+  Future<void> initDynamicLinks() async {
+    /// Handle Firebase links - these don't react to ESR links but we check just in case
+    // final PendingDynamicLinkData? initialDeepLink = await FirebaseDynamicLinks.instance.getInitialLink();
+
+    // if (initialDeepLink != null) {
+    //   LogHelper.d('initial link: ${initialDeepLink.link}');
+    //   if (!initialDeepLink.link.isEsr()) {
+    //     _debugShowMessage('initial firebase link: ${initialDeepLink.link}');
+    //     add(DeeplinkEvent.incomingFirebaseDeepLink(initialDeepLink.link));
+    //   } else {
+    //     _debugShowMessage('initial esr link: ${initialDeepLink.link}');
+    //   }
+    // }
+
+    final initialUriString = await _appLinks.getInitialAppLinkString();
+    if (initialUriString != null) {
+      if (SigningRequestManager.isValidESRScheme(initialUriString)) {
+        add(DeeplinkEvent.incomingESRLink(initialUriString));
+      } else {
+        final uri = Uri.parse(initialUriString);
+        add(DeeplinkEvent.incomingFirebaseDeepLink(uri));
+      }
     }
+
+    _linkSubscription = _appLinks.stringLinkStream.listen((uriString) {
+      // Note: We must listen to strings, not Uris; Uri parsing loses case
+      // and ESR signing requests are case sensitive.
+      if (SigningRequestManager.isValidESRScheme(uriString)) {
+        print('>>> esr link detected: $uriString');
+        _debugShowMessage('esr link detected: $uriString');
+        add(DeeplinkEvent.incomingESRLink(uriString));
+      } else {
+        //_debugShowMessage('Not ESR: $uriString');
+        print('non esr link: $uriString');
+      }
+    }, onError: (error) {
+      print('link subscription error $error');
+    });
 
     FirebaseDynamicLinks.instance.onLink.listen((PendingDynamicLinkData dynamicLinkData) {
       LogHelper.d('received link: ${dynamicLinkData.link}');
-      add(DeeplinkEvent.incomingFirebaseDeepLink(dynamicLinkData.link));
+
+      if (!dynamicLinkData.link.isEsr()) {
+        _debugShowMessage('deep link: ${dynamicLinkData.link}');
+        add(DeeplinkEvent.incomingFirebaseDeepLink(dynamicLinkData.link));
+      } else {
+        _debugShowMessage('ESR link: ${dynamicLinkData.link}');
+      }
     }).onError((error) {
       LogHelper.e('Deep link error: $error');
     });
@@ -41,10 +89,7 @@ class DeeplinkBloc extends Bloc<DeeplinkEvent, DeeplinkState> {
   Future<void> _incomingFirebaseDeepLink(_IncomingFirebaseDeepLink event, Emitter<DeeplinkState> emit) async {
     /// fetch data from link.
     final queryParams = event.link.queryParameters;
-    if (queryParams.isNotEmpty &&
-        queryParams.containsKey('code') &&
-        queryParams.containsKey('chain') &&
-        queryParams.containsKey('dao')) {
+    if (queryParams.isNotEmpty && queryParams.containsKey('code') && queryParams.containsKey('chain')) {
       final code = queryParams['code']!;
       final chain = queryParams['chain']!;
       final dao = queryParams['dao']!;
@@ -72,5 +117,11 @@ class DeeplinkBloc extends Bloc<DeeplinkEvent, DeeplinkState> {
       print('error: ${result.asError!.error}');
       print(result.asError!.error);
     }
+  }
+}
+
+extension UriESR on Uri {
+  bool isEsr() {
+    return SigningRequestManager.isValidESRScheme(toString());
   }
 }
