83b320aefc03095cf7d8ad0887a5a6f3fe75ac5d
NIK <nheger@gmail.com>
2023-06-05 13:42:54 +0800

    added asFormattedString

--- lib/core/network/models/token_value.dart
+++ lib/core/network/models/token_value.dart
@@ -11,4 +11,16 @@ class TokenValue {
     final amount = double.parse(components[0]);
     return TokenValue(amount, tokenModel);
   }
+
+  /// Return a blockchain compatible string for this value
+  ///
+  /// e.g. if amount = 4.91 and token precision is 2, symbol is HYPHA
+  /// => "4.91 HYPHA"
+  /// if amount == 5 => "5.00 HYPHA"
+  ///
+  /// This value can be used for all calls that accept an asset type
+  ///
+  String asFormattedString() {
+    return '${amount.toStringAsFixed(tokenModel.symbol.precision)} ${tokenModel.symbol.symbol}';
+  }
 }
