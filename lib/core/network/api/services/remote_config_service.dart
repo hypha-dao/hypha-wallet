589301044b81bc999d0cfa6ba044e880d49f93c3
NIK <nheger@gmail.com>
2023-07-06 00:40:59 +0800

    remote config fix

--- lib/core/network/api/services/remote_config_service.dart
+++ lib/core/network/api/services/remote_config_service.dart
@@ -11,6 +11,7 @@ class RemoteConfigService {
   static Future<RemoteConfigService> initialized() async {
     final rc = RemoteConfigService();
     await rc.setDefaults();
+    await FirebaseRemoteConfig.instance.fetchAndActivate();
     return rc;
   }
 
@@ -82,7 +83,7 @@ class RemoteConfigService {
         'telos': {
           'name': 'Telos',
           'endpoint': 'https://mainnet.telos.net',
-          'fastEndpoint': 'https://telos.greymass.com',
+          'fastEndpoint': 'https://mainnet.telos.net',
           'loginContract': 'eosio.login',
           'loginAction': 'loginuser',
           'logoutAction': 'deletelogin',
