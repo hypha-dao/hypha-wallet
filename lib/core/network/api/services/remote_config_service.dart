75980f1f032e69566f85f6d8ae4f569e2b57ed49
Gerardo Guijarro <gguijarro_c@chewy.com>
2023-07-24 14:19:23 -0600

    wallet-162: Fix memo

--- lib/core/network/api/services/remote_config_service.dart
+++ lib/core/network/api/services/remote_config_service.dart
@@ -179,10 +179,10 @@ class RemoteConfigService {
       'accountCreatorEndpoint': 'http://34.236.29.152:9108',
       'profileServiceEndpoint': 'http://34.236.29.152:9109',
       'profileService': json.encode({
-        'telos': pppConfig.getProfileSeriviceConfig(Network.telos),
-        'telosTestnet': pppConfig.getProfileSeriviceConfig(Network.telosTestnet),
-        'eos': pppConfig.getProfileSeriviceConfig(Network.eos),
-        'eosTestnet': pppConfig.getProfileSeriviceConfig(Network.eosTestnet),
+        'telos': pppConfig.getProfileServiceConfig(Network.telos),
+        'telosTestnet': pppConfig.getProfileServiceConfig(Network.telosTestnet),
+        'eos': pppConfig.getProfileServiceConfig(Network.eos),
+        'eosTestnet': pppConfig.getProfileServiceConfig(Network.eosTestnet),
       }),
       'signUpEnabled': false,
       'walletEnabled': false,
@@ -193,14 +193,14 @@ class RemoteConfigService {
   }
 
   Future<Map<String, dynamic>> loadProfileServiceConfig() async {
-    final configString = await rootBundle.loadString('assets/config/profile_serivce/config.json');
+    final configString = await rootBundle.loadString('assets/config/profile_service/config.json');
     final configJson = jsonDecode(configString);
     return configJson as Map<String, dynamic>;
   }
 }
 
 extension MapExtensions on Map<String, dynamic> {
-  Map<String, dynamic> getProfileSeriviceConfig(Network network) {
+  Map<String, dynamic> getProfileServiceConfig(Network network) {
     final networkMap = {
       Network.telos: 'prod',
       Network.telosTestnet: 'test',
