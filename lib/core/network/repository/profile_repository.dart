088ac08da66ffdf0de1ec8412d01ed9b9c4ce130
NIK <nheger@gmail.com>
2023-08-03 09:34:56 +0800

    ppp cache service has network support

--- lib/core/network/repository/profile_repository.dart
+++ lib/core/network/repository/profile_repository.dart
@@ -2,10 +2,8 @@ import 'package:hypha_wallet/core/error_handler/model/hypha_error.dart';
 import 'package:hypha_wallet/core/error_handler/model/hypha_error_type.dart';
 import 'package:hypha_wallet/core/network/api/endpoints.dart';
 import 'package:hypha_wallet/core/network/api/services/remote_config_service.dart';
-import 'package:hypha_wallet/core/network/models/network.dart';
 import 'package:hypha_wallet/core/network/models/network_extension.dart';
 import 'package:hypha_wallet/core/network/models/user_profile_data.dart';
-import 'package:hypha_wallet/core/network/networking_manager.dart';
 import 'package:hypha_wallet/ui/architecture/result/result.dart';
 import 'package:hypha_wallet/ui/profile/interactor/profile_data.dart';
 
@@ -14,14 +12,14 @@ class ProfileService {
 
   ProfileService(this._remoteConfigService);
 
-  Future<Result<ProfileData, HyphaError>> getProfile(UserProfileData user, Network network) async {
+  Future<Result<ProfileData, HyphaError>> getProfile(UserProfileData user) async {
     final url =
-        '${_remoteConfigService.profileServiceEndpoint}${Endpoints.pppProfile}/${user.accountName}?network=${network.name}';
+        '${_remoteConfigService.profileServiceCacheEndpoint(user.network)}${Endpoints.pppProfile}/${user.accountName}';
     try {
       final response = await user.network.manager.get(url);
       if (response.statusCode == 200) {
         final map = Map<String, dynamic>.from(response.data);
-        return Result.value(ProfileData.fromJson(map, network, []));
+        return Result.value(ProfileData.fromJson(map, user.network, []));
       } else {
         print('get profile status error: ${response.statusCode} ${response.statusMessage}');
         return Result.error(HyphaError(type: HyphaErrorType.api, message: 'server error ${response.statusMessage}'));
