59025b9741636d108cbf4db9f340a37831e3733c
NIK <nheger@gmail.com>
2023-08-03 08:18:14 +0800

    no extending network manager

--- lib/core/network/repository/profile_repository.dart
+++ lib/core/network/repository/profile_repository.dart
@@ -3,20 +3,22 @@ import 'package:hypha_wallet/core/error_handler/model/hypha_error_type.dart';
 import 'package:hypha_wallet/core/network/api/endpoints.dart';
 import 'package:hypha_wallet/core/network/api/services/remote_config_service.dart';
 import 'package:hypha_wallet/core/network/models/network.dart';
+import 'package:hypha_wallet/core/network/models/network_extension.dart';
+import 'package:hypha_wallet/core/network/models/user_profile_data.dart';
 import 'package:hypha_wallet/core/network/networking_manager.dart';
 import 'package:hypha_wallet/ui/architecture/result/result.dart';
 import 'package:hypha_wallet/ui/profile/interactor/profile_data.dart';
 
-class ProfileService extends NetworkingManager {
+class ProfileService {
   final RemoteConfigService _remoteConfigService;
 
-  ProfileService(this._remoteConfigService) : super(_remoteConfigService.profileServiceEndpoint);
+  ProfileService(this._remoteConfigService);
 
-  Future<Result<ProfileData, HyphaError>> getProfile(String accountName, Network network) async {
+  Future<Result<ProfileData, HyphaError>> getProfile(UserProfileData user, Network network) async {
     final url =
-        '${_remoteConfigService.profileServiceEndpoint}${Endpoints.pppProfile}/$accountName?network=${network.name}';
+        '${_remoteConfigService.profileServiceEndpoint}${Endpoints.pppProfile}/${user.accountName}?network=${network.name}';
     try {
-      final response = await get(url);
+      final response = await user.network.manager.get(url);
       if (response.statusCode == 200) {
         final map = Map<String, dynamic>.from(response.data);
         return Result.value(ProfileData.fromJson(map, network, []));
