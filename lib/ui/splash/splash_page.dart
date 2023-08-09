c4ca98f60e73183bc953e26a02b9a37332990e0c
Gerardo Guijarro <gguijarro_c@chewy.com>
2023-08-09 16:45:18 -0600

    Fix splash issue

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:hypha_wallet/core/network/repository/auth_repository.dart';
+import 'package:hypha_wallet/ui/blocs/authentication/authentication_bloc.dart';
 import 'package:hypha_wallet/ui/bottom_navigation/hypha_bottom_navigation.dart';
 import 'package:hypha_wallet/ui/onboarding/onboarding_page.dart';
 import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatefulWidget {
-  /// If true, means we are authenticated and navigate to bottom page: Else navigate to onboarding.
-  final bool isAuthenticated;
-
-  const SplashScreen({super.key, required this.isAuthenticated});
+  const SplashScreen({super.key});
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -28,20 +28,24 @@ class _SplashScreenState extends State<SplashScreen> with TickerProviderStateMix
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Lottie.asset(
-        'assets/animations/hypha_splash.json',
-        controller: _controller,
-        height: MediaQuery.of(context).size.height * 1,
-        animate: true,
-        onLoaded: (composition) {
-          _controller
-            ..duration = composition.duration
-            ..forward().whenComplete(() => Get.offAll(
-                  widget.isAuthenticated ? const HyphaBottomNavigation() : const OnboardingPage(),
-                ));
-        },
-      ),
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+      builder: (context, state) {
+        return Scaffold(
+          body: Lottie.asset(
+            'assets/animations/hypha_splash.json',
+            controller: _controller,
+            height: MediaQuery.of(context).size.height * 1,
+            animate: true,
+            onLoaded: (composition) {
+              _controller
+                ..duration = composition.duration
+                ..forward().whenComplete(() => Get.offAll(
+                      state.authStatus is Authenticated ? const HyphaBottomNavigation() : const OnboardingPage(),
+                    ));
+            },
+          ),
+        );
+      },
     );
   }
 }
