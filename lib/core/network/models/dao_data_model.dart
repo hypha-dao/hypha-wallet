677bb8f3bf8b867e255671609be8353cd51d2c5b
NIK <nheger@gmail.com>
2023-08-12 19:23:44 +0700

    split IPFS hash in usable hash and type

--- lib/core/network/models/dao_data_model.dart
+++ lib/core/network/models/dao_data_model.dart
@@ -2,24 +2,28 @@ class DaoData {
   final String docId;
   final String detailsDaoName;
   final String settingsDaoTitle;
-  final String settingsLogo;
+  final String logoIPFSHash;
+  final String logoType;
   final String settingsDaoUrl;
 
   DaoData({
     required this.docId,
     required this.detailsDaoName,
     required this.settingsDaoTitle,
-    required this.settingsLogo,
+    required this.logoIPFSHash,
+    required this.logoType,
     required this.settingsDaoUrl,
   });
 
   factory DaoData.fromJson(Map<String, dynamic> json) {
     final Map<String, dynamic> settings = json['settings'][0];
+    final logoUrlAndType = settings['settings_logo_s']?.split(':') ?? ['', ''];
     return DaoData(
       docId: json['docId'],
       detailsDaoName: json['details_daoName_n'],
       settingsDaoTitle: settings['settings_daoTitle_s'],
-      settingsLogo: settings['settings_logo_s'] ?? '',
+      logoIPFSHash: logoUrlAndType[0],
+      logoType: logoUrlAndType[1],
       settingsDaoUrl: settings['settings_daoUrl_s'] ?? '',
     );
   }
