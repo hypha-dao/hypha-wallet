54c438e3704a9a31fe996f9fa3100978d42d9151
Zied Dahmani <zied.dahmani2@gmail.com>
2024-09-06 10:58:28 +0100

    perf: use _registerLazySingleton instead

--- lib/core/di/usecases_module.dart
+++ lib/core/di/usecases_module.dart
@@ -4,7 +4,7 @@ void _registerUseCasesModule() {
   _registerFactory(() => CheckAccountAvailabilityUseCase(_getIt<UserAccountRepository>()));
   _registerFactory(() => FindAvailableAccountUseCase(_getIt<UserAccountRepository>()));
   _registerFactory(() => CreateAccountUseCase(_getIt<AuthRepository>(), _getIt<InviteService>()));
-  _registerFactory(() => FetchProfileUseCase(
+  _registerLazySingleton(() => FetchProfileUseCase(
     _getIt<AuthRepository>(),
     _getIt<ProfileService>(),
     _getIt<HyphaSharedPrefs>(),
