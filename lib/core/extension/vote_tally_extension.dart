80193f08012c827766d28dba8f04d0bd5e17ad48
Zied Dahmani <zied.dahmani2@gmail.com>
2024-10-04 08:39:15 +0100

    refactor: call quantityAsDouble

--- lib/core/extension/vote_tally_extension.dart
+++ lib/core/extension/vote_tally_extension.dart
@@ -1,19 +1,13 @@
 extension VoteTallyExtension on Map<String, dynamic> {
   Map<String, dynamic> calculateUnityAndQuorum() {
     if (this['votetally'] != null && this['votetally'].isNotEmpty) {
-      double supply = 0;
       final double abstain = this['votetally'][0]['abstain_votePower_a'].quantityAsDouble();
       final double pass = this['votetally'][0]['pass_votePower_a'].quantityAsDouble();
       final double fail = this['votetally'][0]['fail_votePower_a'].quantityAsDouble();
+
       final double unity = (pass + fail > 0) ? pass / (pass + fail) : 0;
 
-      if (this['details_ballotSupply_a'] != null) {
-        final List<String> supplyParts = this['details_ballotSupply_a'].split(' ');
-        if (supplyParts.isNotEmpty) {
-          supply = double.parse(supplyParts[0]);
-        }
-      }
-
+      final double supply = this['details_ballotSupply_a']?.quantityAsDouble() ?? 0;
       final double quorum = supply > 0 ? (abstain + pass + fail) / supply : 0;
 
       this['unity'] = unity * 100;
