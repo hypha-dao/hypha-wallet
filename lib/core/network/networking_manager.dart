938100ef00f09bc09d5f004c9b12e9c7977981c2
NIK <nheger@gmail.com>
2024-11-19 11:00:51 +0700

    remove retry - not working for our case

--- lib/core/network/networking_manager.dart
+++ lib/core/network/networking_manager.dart
@@ -1,25 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
-import 'package:dio_smart_retry/dio_smart_retry.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:hypha_wallet/core/logging/log_helper.dart';
 import 'package:hypha_wallet/core/network/api/endpoints.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class NetworkingManager extends DioForNative {
   NetworkingManager(String baseUrl) : super() {
-    final retryInterceptor = RetryInterceptor(
-      dio: this,
-      logPrint: (message) {
-        FirebaseCrashlytics.instance.log('Retry call: $message');
-        LogHelper.e('Retry call: $message');
-      },
-      retries: 1, // retry count
-      retryDelays: const [
-        Duration(seconds: 1), // wait 1 sec before first retry
-      ],
-    );
-
     final loggerInterceptor = PrettyDioLogger(
       requestHeader: false,
       requestBody: true,
@@ -29,7 +15,6 @@ class NetworkingManager extends DioForNative {
       compact: true,
     );
 
-    interceptors.add(retryInterceptor);
     if (kIsDebugNetworking) {
       interceptors.add(loggerInterceptor);
     }
